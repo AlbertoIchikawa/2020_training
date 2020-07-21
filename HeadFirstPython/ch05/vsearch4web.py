57616e44d0d204632035ec4cc8871bdfec1c6f11
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-21 15:20:20 +0900

    day12_Chapter6_P264_特定のリクエスト属性をロギングする試運転の実行結果

--- HeadFirstPython/ch05/vsearch4web.py
+++ HeadFirstPython/ch05/vsearch4web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(str(dir(req)), res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, end='|')
 
 
 @app.route('/search4', methods=['POST'])
