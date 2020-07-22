fa1dd7767e160902980c5bac78fd269a56f3832a
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-22 17:37:02 +0900

    day14_ch11_p438_ロギング失敗の試運転の実行結果

--- HeadFirstPython/ch11/vsearch4web.py
+++ HeadFirstPython/ch11/vsearch4web.py
@@ -32,7 +32,10 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = '検索結果:'
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('*****ロギングが失敗しました:', str(err))
     return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title,
                            the_results=results, )
 
