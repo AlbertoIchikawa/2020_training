1a52f3ffb9c4c1e24290c3dce8296dd264c6762f
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-07-31 19:32:47 +0900

    day18_PythonDjangoチュートリアルSPA編(3)を追加します。

---
+++ DjangoVue/tutorial-spa/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <v-btn>マテリアルボタン</v-btn>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+</style>
