43f834ee1ee5ef0fe822fd84edbeb7c51ea8a7d1
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-08-19 15:42:35 +0900

    day22_ハンドラの使い方

--- day22/logging/logger_lesson2.py
+++ day22/logging/logger_lesson2.py
@@ -4,7 +4,11 @@ logger = logging.getLogger(__name__)
 # setLevel で変更
 logger.setLevel(logging.DEBUG)
 
+#ハンドラの取得
+get_handler = logging.FileHandler('logger.log')
+logger.addHandler(get_handler)
+
 
 def do_something():
     logger.info('from logger_lesson2')
-    logger.debug('from logger_lesson2_debug')
+    logger.info('from logger_lesson2_debug')
