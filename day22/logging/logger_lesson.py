8d1f49b9031ee8c4c46614bb7a086a7abe1e3dd8
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-08-19 15:50:46 +0900

    day22_フィルタの設定

--- day22/logging/logger_lesson.py
+++ day22/logging/logger_lesson.py
@@ -1,9 +1,18 @@
 import logging
 
-import logger_lesson2
-
 logging.basicConfig(level=logging.INFO)
 
-logging.info('info')
 
-logger_lesson2.do_something()
+# ログ専用クラスを作成し、logging.Filter の情報を継承させる
+class NoPassFilter(logging.Filter):
+    # フィルタ条件を設定する
+    def filter(self, record):
+        log_message = record.getMessage()
+        return 'password' not in log_message
+
+
+logger = logging.getLogger(__name__)
+# logger プロパティにフィルタ情報を追加する
+logger.addFilter(NoPassFilter())
+logger.info('from main')
+logger.info('from main password = "test')
