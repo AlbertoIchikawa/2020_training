0cc63f46a927cdaed87c3d6ae0b6741ade254ab3
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-08-19 16:10:44 +0900

    day22_dictConfigの使い方

--- day22/logging/logger_lesson.py
+++ day22/logging/logger_lesson.py
@@ -1,18 +1,46 @@
-import logging
+import logging.config
 
-logging.basicConfig(level=logging.INFO)
+import logger_lesson2
+
+logging.config.dictConfig({
+    'version': 1,
+
+    # フォーマットの設定
+    'formatters': {
+        'customFormat': {
+            'format': 'format=%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+        },
+    },
+    # ハンドラの設定
+    'handlers': {
+        'customStreamHandler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'customFormat',
+            'level': logging.DEBUG
+        }
+    },
+
+    # ロガーの対象一覧
+    'root': {
+        'handlers': ['customStreamHandler'],
+        'level': logging.WARNING,
+    },
+    'loggers': {
+        'outputLogging': {
+            'handlers': ['customStreamHandler'],
+            'level': logging.DEBUG,
+            'propagate': 0
+        }
+    }
+})
 
 
-# ログ専用クラスを作成し、logging.Filter の情報を継承させる
-class NoPassFilter(logging.Filter):
-    # フィルタ条件を設定する
-    def filter(self, record):
-        log_message = record.getMessage()
-        return 'password' not in log_message
+logger = logging.getLogger('__name__')
 
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warning message')
+logger.error('error message')
+logger.critical('critical message')
 
-logger = logging.getLogger(__name__)
-# logger プロパティにフィルタ情報を追加する
-logger.addFilter(NoPassFilter())
-logger.info('from main')
-logger.info('from main password = "test')
+logger_lesson2.do_something()
--- day22/logging/logger_lesson2.py
+++ day22/logging/logger_lesson2.py
@@ -1,14 +1,11 @@
 import logging
 
-logger = logging.getLogger(__name__)
-# setLevel で変更
-logger.setLevel(logging.DEBUG)
-
-#ハンドラの取得
-get_handler = logging.FileHandler('logger.log')
-logger.addHandler(get_handler)
-
 
 def do_something():
-    logger.info('from logger_lesson2')
-    logger.info('from logger_lesson2_debug')
+    # outputLogging を入れてみる
+    logger = logging.getLogger('outputLogging')
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warning('warning message')
+    logger.error('error message')
+    logger.critical('critical message')
