26b3c4d0959fb4ce1dd617b1fac79ebc6d4ba894
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-08-31 19:14:30 +0900

    Assignment１の実装(vue.js中心にモックアップ作成)

---
+++ Assignment1/google_news/frontend/src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+<v-card color="grey lighten-4"
+    flat
+    >
+  <v-toolbar flat>
+    <v-btn icon @click="search" color="grey">
+        <v-icon >mdi-magnify</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+    <v-toolbar-title align-center>ヘッドライン</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn @click.stop="dialog = true"><v-avatar></v-avatar></v-btn>
+    <v-dialog
+    v-model="dialog"
+    max-widht="200">
+    </v-dialog>
+  </v-toolbar>
+</v-card>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    search () {
+      this.$router.push('Search')
+    }
+  }
+}
+</script>
