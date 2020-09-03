d0266c961bdf4dc1b5f1c289ba08d9324536ddc2
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-09-03 19:26:55 +0900

    Vuetifyの細かい設定途中

--- Assignment1/google_news/frontend/src/components/HomePage.vue
+++ Assignment1/google_news/frontend/src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-<v-card color="grey lighten-4"
+<!-- <v-card color="grey lighten-4"
     flat
     >
   <v-toolbar flat>
@@ -11,12 +11,21 @@
     <v-spacer></v-spacer>
     <v-btn @click.stop="dialog = true"><v-avatar></v-avatar></v-btn>
     <div>{{user.data}}</div>
-    <!-- <v-dialog
-    v-model="dialog"
-    max-widht="200">
-    </v-dialog> -->
   </v-toolbar>
 </v-card>
+<v-divider></v-divider> -->
+<v-row justify="center" align-content="center">
+  <v-col cols=12 sm=12 md=12 lg=12 xl=12 v-for="n in 30" :key="n">
+  <v-card>
+  <v-col class="text-left">
+    <v-card-title>
+    title
+  </v-card-title></v-col>
+   <v-col class="text-right"><v-img tile height="100px" width="100px" src="https://2.bp.blogspot.com/-VJOwedxrDa4/WWXXWuTIarI/AAAAAAABFho/l09dY2mf-qs1atvIob4Fgiri45FB6MKCgCLcBGAs/s800/website_news.png"></v-img>
+  </v-col>
+</v-card>
+</v-col>
+</v-row>
 </template>
 
 <script>
